65b64260d92f72054a8d51354a1c24191239f15f
emilyvon <emilyfung86@hotmail.com>
2016-02-23 23:51:41 +1030

    initial commit

---
+++ navigationcontroller-test/GoToRootViewController.swift
@@ -0,0 +1,39 @@
+//
+//  GoToRootViewController.swift
+//  navigationcontroller-test
+//
+//  Created by Mengying Feng on 1/02/2016.
+//  Copyright © 2016 Mengying Feng. All rights reserved.
+//
+
+import UIKit
+
+class GoToRootViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func gotoroot(sender: AnyObject) {
+        
+        navigationController?.popToRootViewControllerAnimated(true)
+        
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ navigationcontroller-test/YellowVC.swift
@@ -0,0 +1,32 @@
+//
+//  YellowVC.swift
+//  navigationcontroller-test
+//
+//  Created by Mengying Feng on 1/02/2016.
+//  Copyright © 2016 Mengying Feng. All rights reserved.
+//
+
+import UIKit
+
+class YellowVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    
+    }
+    
+    
+    
+    @IBAction func pop(sender: AnyObject) {
+    
+        self.navigationController?.popViewControllerAnimated(true)
+        
+    
+    }
+    
+    
+    
+    
+}
